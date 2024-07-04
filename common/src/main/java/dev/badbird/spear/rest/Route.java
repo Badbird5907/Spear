27ffb55fd8967471caa572604942c0e97420c855
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-07-04 14:12:50 -0400

    forgot to add method target to route annotation

--- common/src/main/java/dev/badbird/spear/rest/Route.java
+++ common/src/main/java/dev/badbird/spear/rest/Route.java
@@ -4,7 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 public @interface Route {
     /**
