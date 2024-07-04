a36d166727503284b50e0d604b3f4dae664c5a32
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-07-04 14:20:34 -0400

    ignore abstract and interfaces for SpearGuiceAdapter

--- server/src/main/java/dev/badbird/spear/inject/SpearGuiceAdapter.java
+++ server/src/main/java/dev/badbird/spear/inject/SpearGuiceAdapter.java
@@ -6,12 +6,15 @@ import dev.badbird.spear.http.SpearHandler;
 import lombok.RequiredArgsConstructor;
 import org.reflections.Reflections;
 
+import java.lang.reflect.Modifier;
+
 @RequiredArgsConstructor
 public class SpearGuiceAdapter {
     private final Spear spear;
 
     public Spear registerPackage(String packageName, Injector injector) {
         for (Class<? extends SpearHandler> aClass : new Reflections(packageName).getSubTypesOf(SpearHandler.class)) {
+            if (Modifier.isAbstract(aClass.getModifiers()) || Modifier.isInterface(aClass.getModifiers())) continue;
             Object instance = injector.getInstance(aClass);
             System.out.println("Registering " + instance.getClass().getName());
             spear.register((SpearHandler) instance);
